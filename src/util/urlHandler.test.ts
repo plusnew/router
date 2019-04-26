ffe26a21a38e6c103b97df2d158fd58971cff4af
plusgut <github@webentwickler2-0.de>
2019-04-26 19:30:07 +0200

    [urlHandler] added serializer and deserializer

---
+++ src/util/urlHandler.test.ts
@@ -0,0 +1,228 @@
+import { parseUrl, createUrl } from './urlHandler';
+import { RouteParamsSpec } from 'types/mapper';
+
+describe('urlHandler', () => {
+  it('namespace missmatch', () => {
+    expect(() =>
+      parseUrl('bar', {}, createUrl('foo', {}, {})),
+    ).toThrow(new Error('Can not parse url /foo for wrong namespace bar'));
+  });
+
+  describe('missing parameter', () => {
+    it('parseUrl', () => {
+      const spec: RouteParamsSpec = {
+        foo: 'string',
+        bar: 'string',
+      };
+
+      expect(() =>
+        parseUrl('namespace', spec, '/namespace?foo=bar')
+      ).toThrow(new Error('The url /namespace?foo=bar is missing the parameter bar'));
+    });
+
+    it('createUrl', () => {
+      const spec: RouteParamsSpec = {
+        foo: 'string',
+        bar: 'string',
+      };
+
+      expect(() =>
+        createUrl('namespace', spec, { foo: 'fooValue' }),
+      ).toThrow(new Error('Could not create url for namespace, the property bar was missing'));
+    });
+  });
+
+  describe('string', () => {
+    it('basic', () => {
+      const spec: RouteParamsSpec = {
+        foo: 'string',
+      };
+
+      const parameter = {
+        foo: 'fooValue',
+      };
+
+      expect(
+        parseUrl('namespace', spec, createUrl('namespace', spec, parameter)),
+      ).toEqual(parameter);
+    });
+
+    it('empty String', () => {
+      const spec: RouteParamsSpec = {
+        foo: 'string',
+      };
+
+      const parameter = {
+        foo: '',
+      };
+
+      expect(
+        parseUrl('namespace', spec, createUrl('namespace', spec, parameter)),
+      ).toEqual(parameter);
+    });
+
+    it('createUrl with invalid type', () => {
+      const spec: RouteParamsSpec = {
+        foo: 'string',
+      };
+
+      expect(() =>
+        createUrl('namespace', spec, { foo: 42 }),
+      ).toThrow(new Error('Could not create url for property foo with value 42, it is not of the correct type string'));
+    });
+
+    it('basic with multiple', () => {
+      const spec: RouteParamsSpec = {
+        foo: 'string',
+        bar: 'string',
+      };
+
+      const parameter = {
+        foo: 'fooValue',
+        bar: 'barValue',
+      };
+
+      expect(
+        parseUrl('namespace', spec, createUrl('namespace', spec, parameter)),
+      ).toEqual(parameter);
+    });
+
+    it('weird characters', () => {
+      const spec: RouteParamsSpec = {
+        foo: 'string',
+        bar: 'string',
+      };
+
+      const parameter = {
+        foo: 'foo&Value',
+        bar: 'barValue',
+      };
+
+      expect(
+        parseUrl('namespace', spec, createUrl('namespace', spec, parameter)),
+      ).toEqual(parameter);
+    });
+  });
+
+  describe('number', () => {
+    it('basic', () => {
+      const spec: RouteParamsSpec = {
+        foo: 'number',
+      };
+
+      const parameter = {
+        foo: 23,
+      };
+
+      expect(
+        parseUrl('namespace', spec, createUrl('namespace', spec, parameter)),
+      ).toEqual(parameter);
+    });
+
+    it('createUrl with invalid type', () => {
+      const spec: RouteParamsSpec = {
+        foo: 'number',
+      };
+
+      expect(() =>
+        createUrl('namespace', spec, { foo: 'fooValue' }),
+      ).toThrow(new Error('Could not create url for property foo with value fooValue, it is not of the correct type number'));
+    });
+
+    it('parseUrl with invalid type', () => {
+      const spec: RouteParamsSpec = {
+        foo: 'number',
+      };
+
+      expect(() =>
+        parseUrl('namespace', spec, '/namespace?foo=invalid'),
+      ).toThrow(new Error('The url /namespace?foo=invalid has incorrect parameter foo, it is not parsable as number'));
+    });
+  });
+
+  describe('date', () => {
+    it('basic', () => {
+      const spec: RouteParamsSpec = {
+        foo: 'date',
+      };
+
+      const parameter = {
+        foo: new Date(),
+      };
+
+      expect(
+        parseUrl('namespace', spec, createUrl('namespace', spec, parameter)),
+      ).toEqual(parameter);
+    });
+
+    it('createUrl with invalid type', () => {
+      const spec: RouteParamsSpec = {
+        foo: 'date',
+      };
+
+      expect(() =>
+        createUrl('namespace', spec, { foo: 'fooValue' }),
+      ).toThrow(new Error('Could not create url for property foo with value fooValue, it is not of the correct type date'));
+    });
+
+    it('parseUrl with invalid type', () => {
+      const spec: RouteParamsSpec = {
+        foo: 'date',
+      };
+
+      expect(() =>
+        parseUrl('namespace', spec, '/namespace?foo=invalid'),
+      ).toThrow(new Error('The url /namespace?foo=invalid has incorrect parameter foo, it is not parsable as date'));
+    });
+  });
+
+  describe('boolean', () => {
+    it('true', () => {
+      const spec: RouteParamsSpec = {
+        foo: 'boolean',
+      };
+
+      const parameter = {
+        foo: true,
+      };
+
+      expect(
+        parseUrl('namespace', spec, createUrl('namespace', spec, parameter)),
+      ).toEqual(parameter);
+    });
+
+    it('false', () => {
+      const spec: RouteParamsSpec = {
+        foo: 'boolean',
+      };
+
+      const parameter = {
+        foo: false,
+      };
+
+      expect(
+        parseUrl('namespace', spec, createUrl('namespace', spec, parameter)),
+      ).toEqual(parameter);
+    });
+
+    it('createUrl with invalid type', () => {
+      const spec: RouteParamsSpec = {
+        foo: 'boolean',
+      };
+
+      expect(() =>
+        createUrl('namespace', spec, { foo: 'fooValue' }),
+      ).toThrow(new Error('Could not create url for property foo with value fooValue, it is not of the correct type boolean'));
+    });
+
+    it('parseUrl with invalid type', () => {
+      const spec: RouteParamsSpec = {
+        foo: 'boolean',
+      };
+
+      expect(() =>
+        parseUrl('namespace', spec, '/namespace?foo=invalid'),
+      ).toThrow(new Error('The url /namespace?foo=invalid has incorrect parameter foo, it is not parsable as boolean'));
+    });
+  });
+});
--- src/util/urlHandler.ts
+++ src/util/urlHandler.ts
@@ -7,6 +7,33 @@ const PARAMETER_DELIMITER = '&';
 const PARAMETER_PARAMETERVALUE_DELIMITER = '=';
 
 const serializer = {
+  boolean(value: unknown) {
+    if (typeof value === 'boolean') {
+      return value.toString();
+    }
+    throw new Error('Invalid type');
+  },
+  number(value: unknown) {
+    if (typeof value === 'number') {
+      return value.toString();
+    }
+    throw new Error('Invalid type');
+  },
+  date(value: unknown) {
+    if (value instanceof Date) {
+      return this.string(value.toISOString());
+    }
+    throw new Error('Invalid type');
+  },
+  string(value: unknown) {
+    if (typeof value === 'string') {
+      return encodeURIComponent(value);
+    }
+    throw new Error('Invalid type');
+  },
+};
+
+const deserializer = {
   boolean(value: string) {
     if (value === 'true') {
       return true;
@@ -26,45 +53,48 @@ const serializer = {
     return result;
   },
   date(value: string) {
-    const date = new Date(this.number(value));
+    const date = new Date(this.string(value));
     if (isNaN(date.getTime()) === true) {
       throw new Error(`${value} is not a valid date`);
     }
     return date;
   },
   string(value: string) {
-    return value;
+    return decodeURIComponent(value);
   },
 };
-
-function convert(value: unknown) {
-  if (value instanceof Date) {
-    return value.getTime().toString();
-  }
-  return value;
-}
 
 export function isNamespaceActive(namespace: string, url: string) {
   const [path] = url.split(NAMESPACE_PARAMETER_DELIMITER);
   return formatPath(path) === formatPath(namespace);
 }
 
-export function createUrl <Spec extends RouteParamsSpec>(namespace: string, spec: Spec, params: any) {
-  return (Object.keys(spec)).reduce((previousValue, specKey, index) => {
+export function createUrl <Spec extends RouteParamsSpec>(namespace: string, spec: Spec, params: SpecToType<Spec>) {
+  return (Object.entries(spec)).reduce((previousValue, [propertyName, specType], index) => {
     let result = previousValue;
     if (index === 0) {
       result += NAMESPACE_PARAMETER_DELIMITER;
     } else {
       result += PARAMETER_DELIMITER;
     }
-    result += `${specKey}${PARAMETER_PARAMETERVALUE_DELIMITER}${convert(params[specKey])}`;
-    return result;
+
+    if (propertyName in params) {
+      try {
+        result += `${propertyName}${PARAMETER_PARAMETERVALUE_DELIMITER}${serializer[specType](params[propertyName])}`;
+        return result;
+      } catch (error) {
+        throw new Error(
+          `Could not create url for property ${propertyName} with value ${params[propertyName]}, it is not of the correct type ${specType}`,
+        );
+      }
+    }
+    throw new Error(`Could not create url for ${namespace}, the property ${propertyName} was missing`);
   }, PATH_DELIMITER + namespace);
 }
 
 export function parseUrl <Spec extends RouteParamsSpec>(namespace: string, spec: Spec, url: string) {
   if (isNamespaceActive(namespace, url) === false) {
-    throw new Error('Can not parse Url for wrong namespace');
+    throw new Error(`Can not parse url ${url} for wrong namespace ${namespace}`);
   }
 
   const [, paramUrlPart] = url.split(NAMESPACE_PARAMETER_DELIMITER);
@@ -77,9 +107,9 @@ export function parseUrl <Spec extends RouteParamsSpec>(namespace: string, spec:
     if (paramKey) {
       if (paramKey in spec) {
         try {
-          result[paramKey] = serializer[spec[paramKey]](paramValue);
+          result[paramKey] = deserializer[spec[paramKey]](paramValue);
         } catch (err) {
-          throw new Error(`The url ${url} has incorrect parameter ${paramKey}`);
+          throw new Error(`The url ${url} has incorrect parameter ${paramKey}, it is not parsable as ${spec[paramKey]}`);
         }
       } else {
         throw new Error(`The url ${url} has unknown parameter ${paramKey}`);
