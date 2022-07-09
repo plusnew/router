d0b436bf3f6a0c52befc28c97e53ed368449b81d
plusgut <github@webentwickler2-0.de>
2022-07-09 19:41:01 +0200

    feat(routeTypes): add route types

--- src/routeHandler/index.tsx
+++ src/routeHandler/index.tsx
@@ -7,16 +7,34 @@ type Route<T> = {
     | { isActive: true; isActiveAsParent: false; parameter: T }
     | { isActive: false; isActiveAsParent: true; parameter: T }
     | { isActive: false; isActiveAsParent: false };
+
+  createChildRoute: <U extends string, V extends parameterSpecTemplate>(
+    namespace: U,
+    parameterSpec: V
+  ) => Route<T & { [namespace in U]: parameterSpecToType<V> }>;
 };
 
+const createRouteFactory = function <T>(
+  parents: [string, parameterSpecTemplate][]
+) {
+  return function <U extends string, V extends parameterSpecTemplate>(
+    namespace: U,
+    parameterSpec: V
+  ): Route<T & { [namespace in U]: parameterSpecToType<V> }> {
+    return {
+      parseUrl: () => ({ isActive: false, isActiveAsParent: false }),
+      createChildRoute: createRouteFactory<
+        T & { [namespace in U]: parameterSpecToType<V> }
+      >([...parents, [namespace, parameterSpec]]),
+    };
+  };
+};
 export const createRoute = function <
   T extends string,
   U extends parameterSpecTemplate
 >(
-  _namespace: T,
-  _parameterSpec: U
+  namespace: T,
+  parameterSpec: U
 ): Route<{ [namespace in T]: parameterSpecToType<U> }> {
-  return {
-    parseUrl: () => ({ isActive: false, isActiveAsParent: false }),
-  };
+  return createRouteFactory([])(namespace, parameterSpec);
 };
